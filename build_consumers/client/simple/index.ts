8d399d21932e9b0ad89e0ee22c608d25e08da72d
Roi ben haim (rb136m) <rb136m@att.com>
2018-11-27 06:51:17 +0200

    no message

--- build_consumers/client/simple/index.ts
+++ build_consumers/client/simple/index.ts
@@ -3,15 +3,30 @@ import {
     MethodType
 } from '@methodus/client';
 
-import { Simple } from '../../../build_path/@client/simple/index';
-
+import { Simple } from '@client/simple';
+import { Models } from '@client/models';
+// import { Inherit } from '@client/inherit';
 
 (async () => {
-
-
-    setTimeout(async () => {    
+    setTimeout(async () => {
         const result = await Simple.get('1111');
         console.log(result);
-    }, 5000)
+
+
+
+        // const result2 = await Models.get('1111');
+        // console.log(result2);
+    }, 1000 * 10);
+
+    setTimeout(async () => {
+
+        debugger;
+        const result1 = await Models.get('1111');
+        console.log(result1);
+
+        // const result2 = await Models.get('1111');
+        // console.log(result2);
+    }, 5000);
+
 
 })();
--- build_consumers/client/simple/server/index.ts
+++ build_consumers/client/simple/server/index.ts
@@ -1,11 +1,15 @@
 
 import {
     ServerConfiguration, PluginConfiguration,
-    ConfiguredServer, ServerType,
+    ConfiguredServer, ServerType, MethodType, ClientConfiguration,
 } from '@methodus/server';
 import * as path from 'path';
 
+import { Simple } from '@server/simple';
+
+
 @ServerConfiguration(ServerType.Express, { port: process.env.PORT || 6690 })
+@ClientConfiguration(Simple, MethodType.Mock, ServerType.Express)
 @PluginConfiguration(path.join(__dirname, 'static'), { path: '/' })
 class SetupServer extends ConfiguredServer {
     constructor() {
