affc5169dff432a445a96aaca43bea2bb4ec47ed
Roi ben haim (rb136m) <rb136m@att.com>
2019-01-30 07:28:20 +0200

    no message

--- build.ts
+++ build.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-
+process.env.NODE_LOG_SILENT = 'true';
 import { ServerBuilder } from './src/build.server';
 (async () => {
     await ServerBuilder();
--- client.ts
+++ client.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-
+process.env.NODE_LOG_SILENT = 'true';
 import { ClientBuilder } from './src/build.client';
 (async () => {
     await ClientBuilder();
