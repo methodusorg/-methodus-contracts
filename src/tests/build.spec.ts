9de87748bb91f963f1af4d5ec8868d7f5396515b
Roi ben haim <roi.benhaim@gmail.com>
2019-11-01 21:23:32 +0200

    no message

--- src/tests/build.spec.ts
+++ src/tests/build.spec.ts
@@ -1,30 +1,43 @@
-import { Expect, Test, TestCase, TestFixture, Timeout, Focus } from 'alsatian';
 import { Builder } from '../build.functions';
 import * as path from 'path';
 
+describe('Build server contracts', () => {
+    ['simple', 'models', 'inherit'].forEach((contract) => {
+        test('Build server contracts', async () => {
+            process.chdir(path.join(__dirname, '..', '..'));// reset the cwd, since it changes when generating cotracts
+            const result = await Builder(path.join(process.cwd(), `/build_vars/${contract}/build.json`));
+            expect(result).toBeDefined();
+        });
+    });
+});
 
-@TestFixture('Build server contracts')
-export class ServerTests {
-    @Timeout(60 * 1000 * 10)
-    @Test('Build all flavours')
-    @TestCase('/build_vars/simple/build.json')
-    @TestCase('/build_vars/models/build.json')
-    @TestCase('/build_vars/inherit/build.json')
-    public async testContract(contract) {
-        process.chdir(path.join(__dirname, '..', '..'));// reset the cwd, since it changes when generating cotracts
-        const result = await Builder(path.join(process.cwd(), contract));
-        Expect(result).toBeTruthy();
-    }
+// @TestCase('/build_vars/simple/build.json')
+// // @TestCase()
+// // @TestCase('/build_vars/inherit/build.json')
+
+// });
 
 
-    @Test('Build from args')
-    @TestCase('/build_vars/simple/build.json')
-    @TestCase('/build_vars/inherit/build.json')
-    @TestCase('/build_vars/models/build.json')
-    public async testArgContract(contract) {
-        process.chdir(path.join(__dirname, '..', '..'));// reset the cwd, since it changes when generating cotracts
-        process.argv[2] = contract;
-        await Builder();
-    }  
+// @TestFixture('Build server contracts')
+// export class ServerTests {
+//     @Timeout(60 * 1000 * 10)
+//     @Test('Build all flavours')
 
-}
+//     public async testContract(contract) {
+//         process.chdir(path.join(__dirname, '..', '..'));// reset the cwd, since it changes when generating cotracts
+//         const result = await Builder(path.join(process.cwd(), contract));
+//         Expect(result).toBeTruthy();
+//     }
+
+
+//     // @Test('Build from args')
+//     // @TestCase('/build_vars/simple/build.json')
+//     // @TestCase('/build_vars/inherit/build.json')
+//     // @TestCase('/build_vars/models/build.json')
+//     // public async testArgContract(contract) {
+//     //     process.chdir(path.join(__dirname, '..', '..'));// reset the cwd, since it changes when generating cotracts
+//     //     process.argv[2] = contract;
+//     //     await Builder();
+//     // }  
+
+// }
--- src/tests/client.spec.ts
+++ src/tests/client.spec.ts
@@ -1,38 +1,52 @@
-import { Expect, Test, TestCase, TestFixture, Focus } from 'alsatian';
+
+
 import { Builder } from '../build.functions';
 import * as path from 'path';
 
-@TestFixture('Build client contracts')
-export class ClientTests {
-    @Test('Build client contracts')
-    @TestCase('/build_vars/simple/build.json')
-    @TestCase('/build_vars/inherit/build.json')
-    @TestCase('/build_vars/models/build.json')
-    public async testContract(contract) {
-        let result = false;
 
-        process.chdir(path.join(__dirname, '..', '..'));// reset the cwd, since it changes when generating cotracts
-        console.log(process.cwd());
-
-        await Builder(path.join(process.cwd(), contract), true);
-        result = true;
-
-        Expect(result).toBeTruthy();
-    }
+describe('Build server contracts', () => {
+    ['simple', 'models', 'inherit'].forEach((contract) => {
+        test('Build server contracts', async () => {
+            process.chdir(path.join(__dirname, '..', '..'));// reset the cwd, since it changes when generating cotracts
+            const result = await Builder(path.join(process.cwd(), `/build_vars/${contract}/build.json`), true);
+            expect(result).toBeDefined();
+        });
+    });
+});
 
 
-    @Test('Build from args')
-    @TestCase('/build_vars/simple/build.json')
-    @TestCase('/build_vars/inherit/build.json')
-    @TestCase('/build_vars/models/build.json')
-    public async testArgContract(contract) {
-        process.argv[2] = contract;
-        process.chdir(path.join(__dirname, '..', '..'));// reset the cwd, since it changes when generating cotracts
 
-        let result = false;
-        await Builder();
-        result = true;
-        Expect(result).toBeTruthy();
+// @TestFixture('Build client contracts')
+// export class ClientTests {
+//     @Test('Build client contracts')
+//     @TestCase('/build_vars/simple/build.json')
+//     // @TestCase('/build_vars/inherit/build.json')
+//     // @TestCase('/build_vars/models/build.json')
+//     public async testContract(contract) {
+//         let result = false;
 
-    }
-}
+//         process.chdir(path.join(__dirname, '..', '..'));// reset the cwd, since it changes when generating cotracts
+         
+
+//         await Builder(path.join(process.cwd(), contract), true);
+//         result = true;
+
+//         Expect(result).toBeTruthy();
+//     }
+
+
+//     // @Test('Build from args')
+//     // @TestCase('/build_vars/simple/build.json')
+//     // @TestCase('/build_vars/inherit/build.json')
+//     // @TestCase('/build_vars/models/build.json')
+//     // public async testArgContract(contract) {
+//     //     process.argv[2] = contract;
+//     //     process.chdir(path.join(__dirname, '..', '..'));// reset the cwd, since it changes when generating cotracts
+
+//     //     let result = false;
+//     //     await Builder();
+//     //     result = true;
+//     //     Expect(result).toBeTruthy();
+
+//     // }
+// }
